54a2eca814f814884f54595aa4852463d7966385
Bakker <roybakker17@gmail.com>
2019-06-27 14:45:25 +0200

    Frist

---
+++ js/mixitup-config.js
@@ -0,0 +1,8 @@
+// Mixitup Settings
+/*eslint-disable */
+var containerEl = document.querySelector('#portfolioList');
+var mixer = mixitup(containerEl, {
+  selectors: {
+    control: '[data-mixitup-control]'
+  }
+});
